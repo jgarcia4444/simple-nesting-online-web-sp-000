1e215d542ef2aaaf451f2f5b20e1835262715b17
Jake Garcia <bballwiz4@gmail.com>
2020-02-26 00:13:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,18 +68,22 @@ def adding_matz
 
 	programmer_hash =
  		{
-        :grace_hopper => {
+        	:grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
+        	:alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => {
+        	:dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
+					:yukihiro_matsumoto => {
+					:known_for => "Ruby",
+				  :languages => ["LISP", "C"]
+				}
      }
 
 
@@ -103,7 +107,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 end
 
